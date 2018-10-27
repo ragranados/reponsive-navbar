3cbf3449f85335ffd94f986defa09eefffb25299
00138816@uca.edu.sv <00138816@uca.edu.sv>
2018-10-26 22:23:31 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Toolbar from './components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 
 class App extends Component {
+  state = {
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+
   render() {
+    let sideDrawer;
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      sideDrawer = <SideDrawer />;
+      backdrop = <Backdrop drawerClickHandler={this.drawerToggleClickHandler}/>;
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{height:'100%'}}>
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
+        {sideDrawer}
+        {backdrop}
+        <main style={{ marginTop: '64px' }}>
+          <p>Contenido de la pagina</p>
+        </main>
       </div>
     );
   }
---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import DrawerToggleButton from '../SideDrawer/DrawerToggleButton'
+import './Toolbar.css'
+const toolbar = props => (
+    <header className="toolbar">
+        <nav className="toolbar__navigation">
+            <div>
+                <DrawerToggleButton click = {props.drawerClickHandler}/>
+            </div>
+            <div className="toolbar__logo"> <a href="/" >LOGO</a> </div>
+            <div className="spacer"></div>
+            <div className="toolbar_navigation-items">
+                <ul>
+                    <li> <a href="/" >Usuarios</a></li>
+                    <li> <a href="/" >Usuarios</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default toolbar;
